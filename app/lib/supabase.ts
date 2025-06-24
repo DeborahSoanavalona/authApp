6d6488894206aa8c5a3b20467a43d4265def2e16
DeborahSoanavalona <deborah.razafindrabezandrina@esti.mg>
2025-06-24 11:16:53 +0300

    Basic login page structure

--- app/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { cookies } from 'next/headers'
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
-
-export function createClient() {
-  return createServerActionClient({ cookies })
-}
