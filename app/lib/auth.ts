6e7290d5949fb8e51b4cf191174b98812904c837
SoanavalonaD <deborah.razafindrabezandrina@esti.mg>
2025-07-21 10:11:01 +0300

    email/password method

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,11 +2,20 @@ import { betterAuth } from "better-auth";
 import { Pool } from "pg";
  
 export const auth = betterAuth({
-    database: new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD, 
-  port: Number(process.env.DB_PORT) || 5432,
-})
+  database: new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD, 
+    port: Number(process.env.DB_PORT) || 5432,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }
+  }
 })
