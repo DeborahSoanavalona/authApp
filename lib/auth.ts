4a56c2988a5268325d1a9f44efb60d370b258750
SoanavalonaD <deborah.razafindrabezandrina@esti.mg>
2025-07-23 00:25:40 +0300

    google adapter

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,12 +5,18 @@ import { nextCookies } from "better-auth/next-js";
 import {schema } from "@/db/schema";
  
 export const auth = betterAuth({
-    emailAndPassword: {
-    enabled: true, 
+  socialProviders: {
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID as string, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+    }, 
+  },
+  emailAndPassword: {
+  enabled: true, 
   }, 
-    database: drizzleAdapter(db, {
-        provider: "pg", 
-        schema,
-    }),
-    plugins: [nextCookies()],
+  database: drizzleAdapter(db, {
+    provider: "pg", 
+    schema,
+  }),
+  plugins: [nextCookies()],
 });
